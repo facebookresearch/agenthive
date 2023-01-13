2e5e1e6518cbc6e3b05295059a99885d6084b23c
vmoens <vincentmoens@gmail.com>
2023-01-13 18:18:09 +0000

    minor

--- rlhive/rl_envs.py
+++ rlhive/rl_envs.py
@@ -2,7 +2,6 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-from copy import copy
 
 import numpy as np
 import torch
