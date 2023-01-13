5273ba67fa5906edde3a6c5b7ff0b60dd1bd90e5
vmoens <vincentmoens@gmail.com>
2023-01-13 17:00:31 +0000

    init

--- rlhive/__init__.py
+++ rlhive/__init__.py
@@ -3,8 +3,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .envs import register_franka_envs, register_kitchen_envs
+# from .envs import register_franka_envs, register_kitchen_envs
+#
+# register_franka_envs()
+# register_kitchen_envs()
 
-register_franka_envs()
-register_kitchen_envs()
 from .rl_envs import RoboHiveEnv
