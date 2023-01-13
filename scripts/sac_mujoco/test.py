6c03e9c82ba29bca6446eba3d29c4083f2517b3c
ShahRutav <rutavms@utexas.edu>
2023-01-12 22:27:21 -0600

    test.py updated with another bug

--- scripts/sac_mujoco/test.py
+++ scripts/sac_mujoco/test.py
@@ -17,17 +17,11 @@ def make_transformed_env(
     env = TransformedEnv(env, Compose(R3MTransform('resnet50', in_keys=["pixels"], download=True), FlattenObservation(-2, -1, in_keys=["r3m_vec"])))
     return env
 
-from torchrl.envs import default_info_dict_reader
-reader = default_info_dict_reader(["solved"])
 base_env = RoboHiveEnv("visual_franka_slide_random-v3", device=torch.device('cuda:0'))
+env = base_env
 env = make_transformed_env(base_env)
-env = env.set_info_dict_reader(info_dict_reader=reader)
+print(env)
 with torch.no_grad(), set_exploration_mode("random"):
     td = env.reset()
     td = env.rand_step()
     print(td)
-    #print(td['observation_vector'].shape)
-    #print(td['r3m_vec'].shape)
-    #print(env)
-    #print(td)
-
