a43e2a488c174e0dc677cd433c9b0179db3cfb70
vmoens <vincentmoens@gmail.com>
2023-01-27 16:01:26 +0000

    amend

--- examples/install/install_rlhive.sh
+++ examples/install/install_rlhive.sh
@@ -1,17 +1,21 @@
 #!/bin/zsh
 
+set -e
+
 here=$(pwd)
 module_path=$HOME/modules/
 
-module load cuda/11.6 cudnn/v8.4.1.50-cuda.11.6
+conda env remove -n rlhive -y
 
 conda create -n rlhive -y python=3.8
 conda activate rlhive
 
 python3 -mpip install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cu116
 
+mkdir $module_path
 cd $module_path
 git clone -c submodule.mj_envs/sims/neuromuscular_sim.update=none --branch v0.4dev --recursive https://github.com/vikashplus/mj_envs.git mj_envs
+cd mj_envs
 python3 -mpip install .  # one can also install it locally with the -e flag
 cd $here
 
