318ef6d7bbaff7ff4f9ee321ac165428b8cb7a20
vmoens <vincentmoens@gmail.com>
2023-01-31 08:01:07 +0000

    amend

--- examples/sac.py
+++ examples/sac.py
@@ -428,7 +428,7 @@ def main(args: DictConfig):
                 entropies,
             ) = ([], [], [], [], [], [])
             for _ in range(
-                args.env_per_collector * args.frames_per_batch * args.utd_ratio // args.batch_size
+                args.frames_per_batch * args.utd_ratio // args.batch_size
             ):
                 optim_steps += 1
                 # sample from replay buffer
