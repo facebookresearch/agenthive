5264de4216c3372b1011eb9dd35449b643d85e31
vmoens <vincentmoens@gmail.com>
2023-03-07 13:50:40 +0000

    init

---
+++ examples/train.py
@@ -0,0 +1,13 @@
+"""Entry point for RLHive"""
+import hydra
+from omegaconf import DictConfig
+
+from sac import main as train_sac
+
+
+@hydra.main(config_name="sac_mixed.yaml", config_path="config")
+def main(args: DictConfig):
+    if args.algo == "sac":
+        train_sac(args)
+    else:
+        raise NotImplementedError
