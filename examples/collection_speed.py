1bb46ec750a949041aa5247baeceb17c6f43b1bd
vmoens <vincentmoens@gmail.com>
2023-04-12 10:33:51 +0100

    amend

--- examples/collection_speed.py
+++ examples/collection_speed.py
@@ -13,13 +13,14 @@ import time
 import tqdm
 
 from rlhive.rl_envs import RoboHiveEnv
+from torchrl.collectors.distributed.rpc import RPCDataCollector
 from torchrl.collectors.collectors import MultiaSyncDataCollector, RandomPolicy
 from torchrl.envs import EnvCreator, ParallelEnv, R3MTransform, TransformedEnv
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--num_workers", default=2, type=int)
 parser.add_argument("--num_collectors", default=4, type=int)
-parser.add_argument("--frames_per_batch", default=50, type=int)
+parser.add_argument("--frames_per_batch", default=200, type=int)
 parser.add_argument("--total_frames", default=20_000, type=int)
 parser.add_argument("--r3m", action="store_true")
 parser.add_argument("--env_name", default="franka_micro_random-v3")
@@ -27,10 +28,13 @@ parser.add_argument("--env_name", default="franka_micro_random-v3")
 if __name__ == "__main__":
     args = parser.parse_args()
 
-    penv = ParallelEnv(
-        args.num_workers,
-        EnvCreator(lambda: RoboHiveEnv(args.env_name, device="cuda:0")),
-    )
+    if args.num_workers > 1:
+        penv = ParallelEnv(
+            args.num_workers,
+            EnvCreator(lambda: RoboHiveEnv(args.env_name, device="cuda:0")),
+        )
+    else:
+        penv = RoboHiveEnv(args.env_name, device="cuda:0")
     if "visual" in args.env_name:
         if args.r3m:
             tenv = TransformedEnv(
@@ -45,16 +49,25 @@ if __name__ == "__main__":
         #                               num_iter=1000)
     policy = RandomPolicy(tenv.action_spec)  # some random policy
 
-    devices = [f"cuda:{i}" for i in range(args.num_collectors)]
-    print(devices)
-    collector = MultiaSyncDataCollector(
+    device = "cuda:0"
+
+    slurm_conf = {
+        "timeout_min": 100,
+        "slurm_partition": "train",
+        "slurm_cpus_per_gpu": 12,
+        "slurm_gpus_per_task": 1,
+    }
+
+    collector = RPCDataCollector(
         [tenv] * args.num_collectors,
         policy=policy,
         frames_per_batch=args.frames_per_batch,
         total_frames=args.total_frames,
-        passing_devices=devices,
-        devices=devices,
+        storing_device=device,
         split_trajs=False,
+        sync=True,
+        launcher="submitit",
+        slurm_kwargs=slurm_conf,
     )
     pbar = tqdm.tqdm(total=args.total_frames)
     for i, data in enumerate(collector):
