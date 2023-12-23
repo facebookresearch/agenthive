c029c2b5c4970ea9cc8c5ece928f722aa4c5eef3
vmoens <vincentmoens@gmail.com>
2023-12-23 07:41:51 +0100

    amend

--- rlhive/rl_envs.py
+++ rlhive/rl_envs.py
@@ -4,16 +4,28 @@
 # LICENSE file in the root directory of this source tree.
 
 from torchrl.envs import RoboHiveEnv  # noqa
-from torchrl.envs.transforms import CatTensors, Compose, R3MTransform, \
-    TransformedEnv, FlattenObservation
+from torchrl.envs.transforms import (
+    CatTensors,
+    Compose,
+    FlattenObservation,
+    R3MTransform,
+    TransformedEnv,
+)
 from torchrl.trainers.helpers.envs import LIBS
 
 
 def make_r3m_env(env_name, model_name="resnet50", download=True, **kwargs):
     base_env = RoboHiveEnv(env_name, from_pixels=True, pixels_only=False)
-    vec_keys = [k for k in base_env.observation_spec.keys() if
-                (k not in ("pixels", "state",
-                           "time") and "rwd" not in k and "visual" not in k and "dict" not in k)]
+    vec_keys = [
+        k
+        for k in base_env.observation_spec.keys()
+        if (
+            k not in ("pixels", "state", "time")
+            and "rwd" not in k
+            and "visual" not in k
+            and "dict" not in k
+        )
+    ]
     env = TransformedEnv(
         base_env,
         Compose(
@@ -25,10 +37,7 @@ def make_r3m_env(env_name, model_name="resnet50", download=True, **kwargs):
                 **kwargs,
             ),
             FlattenObservation(-2, -1, in_keys=["pixel_r3m"]),
-            CatTensors(
-                in_keys=["pixel_r3m", *vec_keys],
-                out_key="observation_vector"
-            ),
+            CatTensors(in_keys=["pixel_r3m", *vec_keys], out_key="observation_vector"),
         ),
     )
     return env
